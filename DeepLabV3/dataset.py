5e2b10db08c8d3ec3058c54f147df1b6719d4ad9
Tungyen <cony998820@gmail.com>
2024-06-21 12:01:43 +0800

    a

---
+++ DeepLabV3/dataset.py
@@ -0,0 +1,53 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import cv2
+import os
+
+
+train_dirs = ["jena/", "zurich/", "weimar/", "ulm/", "tubingen/", "stuttgart/",
+              "strasbourg/", "monchengladbach/", "krefeld/", "hanover/",
+              "hamburg/", "erfurt/", "dusseldorf/", "darmstadt/", "cologne/",
+              "bremen/", "bochum/", "aachen/"]
+val_dirs = ["frankfurt/", "munster/", "lindau/"]
+test_dirs = ["berlin", "bielefeld", "bonn", "leverkusen", "mainz", "munich"]
+loc_list = [train_dirs, val_dirs, test_dirs]
+
+mode_to_loc = {"train": 0, "val": 1, "test": 2}
+
+class CityScapes(Dataset):
+    def __init__(self, root='./Dataset/cityscapes', mode='train', transform=None):
+        self.img_path = os.path.join(root, 'leftImg8bit', mode)
+        self.label_path = os.path.join(root, 'gtFine_trainvaltest', 'gtFine', mode)
+        
+        self.img_h = 1024
+        self.img_w = 2048
+
+        self.new_img_h = 512
+        self.new_img_w = 1024
+        self.transform = transform
+        self.datas = []
+        
+        for loc in loc_list[mode_to_loc[mode]]:
+            img_loc_dir = os.path.join(self.img_path, loc)
+            
+            for img_loc_file in os.listdir(img_loc_dir):
+                img_id = img_loc_file.split("_leftImg8bit.png")[0]
+                
+                data = {}
+                data['img_path'] = os.path.join(img_loc_dir, img_loc_file)
+                data['label_path'] = os.path.join(self.label_path, loc, loc+'_'+img_id+'_gtFine_labelIds.png')
+                data['img_id'] = img_id
+                self.datas.append(data)
+                
+    def __len__(self):
+        return len(self.datas)
+    
+    def __getitem__(self, index):
+        data = self.datas[index]
+        img = cv2.imread(data['img_path'], -1)
+        label = cv2.imread(data['label_path'], -1)
+        
+        img, label = self.transform(img, label)
+        
+        
