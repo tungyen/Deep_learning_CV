abe5f056d0d21d348181c9ba0fc3fd52647b198e
tungyen <cony998820@gmail.com>
2025-03-17 02:25:43 +0000

    Update the code style

--- GenAI/VAE_pi/test.py
+++ GenAI/VAE_pi/test.py
@@ -2,37 +2,54 @@ from dataset import *
 from model import *
 import torch
 from PIL import Image
+import argparse
 
-device="cuda"
-model = VAE_PI(5, 2).to(device)
-ckpts = torch.load("ckpts/pi.pt")
-model.load_state_dict(ckpts)
-batch_size = 16
+def test_model_img(args):
+    device = args.device
+    model = VAE_PI(5, args.latent_dim).to(device)
+    ckpts = torch.load(args.ckpts_path)
+    model.load_state_dict(ckpts)
+    batch_size = args.batch_size
 
-img_size = 300
-latent_dim = 2
+    img_size = args.img_size
+    latent_dim = args.latent_dim
 
+    times = args.loops
+    img = np.zeros((img_size, img_size, 3)).astype(np.uint8)
 
-times = 1000
-img = np.zeros((img_size, img_size, 3)).astype(np.uint8)
-
-for _ in range(times):
-    z = torch.randn(batch_size, latent_dim)
-    z = z.to(device)
-    reconstruct = model.decode(z)
-    pos = reconstruct[:, :2]
-    color = reconstruct[:, 2:]
-    pos = torch.clamp(pos, min=0, max=(img_size-1)/img_size).cpu().detach().numpy()
-    color = torch.clamp(color, min=0, max=1).cpu().detach().numpy()
-    for i in range(reconstruct.shape[0]):
-        p = pos[i]
-        c = color[i]
-        p = np.floor(p * img_size+0.5).astype(int)
-        p = tuple(p)
-        c = (c*255).astype(np.uint8)
-        img[p[1], p[0], :] = c
+    for _ in range(times):
+        z = torch.randn(args.batch_size, args.latent_dim)
+        z = z.to(device)
+        reconstruct = model.decode(z)
+        pos = reconstruct[:, :2]
+        color = reconstruct[:, 2:]
+        pos = torch.clamp(pos, min=0, max=(args.img_size-1)/args.img_size).cpu().detach().numpy()
+        color = torch.clamp(color, min=0, max=1).cpu().detach().numpy()
+        for i in range(reconstruct.shape[0]):
+            p = pos[i]
+            c = color[i]
+            p = np.floor(p * args.img_size+0.5).astype(int)
+            p = tuple(p)
+            c = (c*255).astype(np.uint8)
+            img[p[1], p[0], :] = c
+            
+    img = Image.fromarray(img)
+    img.save("img/gen_pi.png")
         
-img = Image.fromarray(img)
-img.save("gen_pi.png")
-        
-        
+def parse_args():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--latent_dim', type=int, default=2)
+    parse.add_argument('--batch_size', type=int, default=16)
+    parse.add_argument('--img_size', type=int, default=300)
+    parse.add_argument('--loops', type=int, default=1000)
+    parse.add_argument('--device', type=str, default="cuda")
+    parse.add_argument('--xs_path', type=str, default="pi_xs.npy")
+    parse.add_argument('--ys_path', type=str, default="pi_ys.npy")
+    parse.add_argument('--img_path', type=str, default="sparse_pi_colored.jpg")
+    parse.add_argument('--ckpts_path', type=str, default="ckpts/pi.pt")
+    args = parse.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    test_model_img(args)    
--- GenAI/VAE_pi/test2.py
+++ GenAI/VAE_pi/test2.py
@@ -7,50 +7,64 @@ from scipy.stats import wasserstein_distance, entropy
 from dataset import *
 from model import *
 import torch
-device="cuda"
-model = VAE_PI(5, 2).to(device)
-ckpts = torch.load("ckpts/pi.pt")
-model.load_state_dict(ckpts)
+import argparse
 
-# Load real data
-xs = np.load("pi_xs.npy")
-ys = np.load("pi_ys.npy")
-image_array = np.array(Image.open("sparse_pi_colored.jpg"))
-rgb_values = image_array[xs, ys]
-real_data = np.column_stack([xs, ys, rgb_values])
 
-latent_dim = 2
-img_size = 300
-def generate_samples(batch_size):
-    z = torch.randn(batch_size, latent_dim)
-    z = z.to(device)
-    reconstruct = model.decode(z)
-    pos = reconstruct[:, :2]
-    color = reconstruct[:, 2:]
-    pos = torch.clamp(pos, min=0, max=(img_size-1)/img_size).cpu().detach().numpy()
-    color = torch.clamp(color, min=0, max=1).cpu().detach().numpy()
+def test_model_distribution(args):
+    device = args.device
+    model = VAE_PI(5, args.latent_dim).to(device)
+    ckpts = torch.load(args.ckpts_path)
+    model.load_state_dict(ckpts)
+
+    # Load real data
+    xs = np.load(args.xs_path)
+    ys = np.load(args.ys_path)
+    image_array = np.array(Image.open(args.img_path))
+    rgb_values = image_array[xs, ys]
+    real_data = np.column_stack([xs, ys, rgb_values])
     
-    pos = np.floor(pos * img_size+0.5).astype(int)
-    color = (color*255).astype(np.uint8)
-    return np.column_stack([pos, color])
+    def generate_samples(args):
+        z = torch.randn(args.batch_size, args.latent_dim)
+        z = z.to(device)
+        reconstruct = model.decode(z)
+        pos = reconstruct[:, :2]
+        color = reconstruct[:, 2:]
+        pos = torch.clamp(pos, min=0, max=(args.img_size-1)/args.img_size).cpu().detach().numpy()
+        color = torch.clamp(color, min=0, max=1).cpu().detach().numpy()
+        
+        pos = np.floor(pos * args.img_size+0.5).astype(int)
+        color = (color*255).astype(np.uint8)
+        return np.column_stack([pos, color])
 
-generated_data = generate_samples(5000)
+    generated_data = generate_samples(args)
 
-for i, label in enumerate(["x", "y", "r", "g", "b"]):
-    plt.figure(figsize=(6, 4))
-    sns.kdeplot(real_data[:, i], label="Real", fill=True)
-    sns.kdeplot(generated_data[:, i], label="Generated", fill=True)
-    plt.title(f"Distribution of {label}")
-    plt.legend()
-    plt.savefig('img/distribute_{}.png'.format(label), dpi=300, bbox_inches='tight')
-    plt.show()
+    for i, label in enumerate(["x", "y", "r", "g", "b"]):
+        plt.figure(figsize=(6, 4))
+        sns.kdeplot(real_data[:, i], label="Real", fill=True)
+        sns.kdeplot(generated_data[:, i], label="Generated", fill=True)
+        plt.title(f"Distribution of {label}")
+        plt.legend()
+        plt.savefig('img/distribute_{}.png'.format(label), dpi=300, bbox_inches='tight')
+        plt.show()
 
-# Compute the Wasserstein distance
-distances = [wasserstein_distance(real_data[:, i], generated_data[:, i]) for i in range(5)]
-print("Wasserstein distances:", distances)
+    # Compute the Wasserstein distance
+    distances = [wasserstein_distance(real_data[:, i], generated_data[:, i]) for i in range(5)]
+    print("Wasserstein distances:", distances)
 
-# Compute the KL divergence
-bins = np.histogram_bin_edges(real_data[:, 0], bins="auto")
-kl_divs = [entropy(np.histogram(real_data[:, i], bins=bins, density=True)[0], 
-                   np.histogram(generated_data[:, i], bins=bins, density=True)[0]) for i in range(5)]
-print("KL divergences:", kl_divs)
+
+def parse_args():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--latent_dim', type=int, default=2)
+    parse.add_argument('--batch_size', type=int, default=5000)
+    parse.add_argument('--img_size', type=int, default=300)
+    parse.add_argument('--device', type=str, default="cuda")
+    parse.add_argument('--xs_path', type=str, default="pi_xs.npy")
+    parse.add_argument('--ys_path', type=str, default="pi_ys.npy")
+    parse.add_argument('--img_path', type=str, default="sparse_pi_colored.jpg")
+    parse.add_argument('--ckpts_path', type=str, default="ckpts/pi.pt")
+    args = parse.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    test_model_distribution(args)
